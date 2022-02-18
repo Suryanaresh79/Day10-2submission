155f6ebfc4bf054ec6fbf1ef72ebec07ecd8728a
Suryanaresh79 <98698880+Suryanaresh79@users.noreply.github.com>
2022-02-18 23:44:54 +0530

    Flip Coin Simulation Problems

---
+++ Day10-Flip Coin Simulation Problems/UC-2/flipCoinSimulator.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+for ((i=0;i<=20;i++))
+do
+coin[i]=$((RANDOM%2))
+if [[ ${coin[i]} -eq 0 ]]
+then
+      heads=$(($heads+1))
+else
+      tails=$(($tails+1))
+fi
+done
+echo "No of times heads : "$heads
+echo "No of times tails : "$tails
